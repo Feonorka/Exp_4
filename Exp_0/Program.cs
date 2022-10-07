7bc4ec85b9d5284f0468c92c7280bff59f53b3c6
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-10-07 23:19:32 +0300

    Описан принцип массивов

--- Exp_0/Program.cs
+++ Exp_0/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string[,] table = new string[2,5];
+// String.Empty
+// table[0,0]   table[0,1]  table[0,2]  table[0,3]  table[0,4]
+// table[1,0]   table[1,1]  table[1,2]  table[1,3]  table[1,4]
+
+table[1,2] = "Слово";
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($"-{table[rows, columns]}-");
+    }
+}
+
+int[,] matrix = new int[3,4];
+for (int rows = 0; rows < matrix.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < matrix.GetLength(1); columns++)
+    {
+        Console.Write($"{matrix[rows,columns]} ");
+    }
+    Console.WriteLine();
+}
