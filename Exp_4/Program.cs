c9f341cd1eb4be023f18a695c01ce601c63993f0
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-10-08 11:26:59 +0300

    Сделаны задачки из лекции

---
+++ Exp_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Фибоначи");
+
+double Fibonachi(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonachi(n-1) + Fibonachi(n-2);
+}
+
+for(int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonachi(i)}");
+}
